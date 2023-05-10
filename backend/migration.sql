13b29f9ff74ea93ac1e59513d72d319c9eeae29f
JosephCarmeli <josephcarmeli@hotmail.com>
2023-05-10 12:27:05 -0500

    WIP

--- backend/migration.sql
+++ backend/migration.sql
@@ -1,13 +1,16 @@
-DROP TABLE IF EXISTS users, login_credentials;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
+  fullname TEXT,
+  email VARCHAR NOT NULL,
   password VARCHAR(50) NOT NULL,
   create_datetime TIMESTAMP NOT NULL
 );
 
-
-INSERT INTO users (username, password, create_datetime) VALUES ('johndoe', 'password123', NOW());
-INSERT INTO users (username, password, create_datetime) VALUES ('janedoe', 'password456', NOW());
-INSERT INTO users (username, password, create_datetime) VALUES ('bobsmith', 'password789', NOW());
+INSERT INTO users (fullname, email, password, create_datetime) 
+VALUES ('John Doe', 'johndoe@example.com', 'password123', NOW());
+INSERT INTO users (fullname, email, password,create_datetime) 
+VALUES ('Jane Doe', 'janedoe@example.com', 'password456', NOW());
+INSERT INTO users (fullname, email, password,create_datetime) 
+VALUES ('Bob Smith', 'bobsmith@example.com', 'password789', NOW());
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import pg from 'pg';
 
 const server = express();
+server.use(express.json());
+
 const port = 3001;
 const pool = new pg.Pool({
-    user: 'joseph',
-    password: '123',
-    database: 'users'
+  user: 'joseph',
+  password: '123',
+  database: 'users'
 });
 
 
 server.listen(port, () => {
-    console.log('Server is listening on port 3001')
+  console.log(`Server is listening on port ${port}`);
 });
