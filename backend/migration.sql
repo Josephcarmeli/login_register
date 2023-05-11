7638f6f8dba1828eee30ed9f7adb1fb11501566f
JosephCarmeli <josephcarmeli@hotmail.com>
2023-05-11 09:58:59 -0500

    WIP

--- backend/migration.sql
+++ backend/migration.sql
@@ -8,3 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
   create_datetime TIMESTAMP NOT NULL
 );
 
+INSERT INTO users (fullname, email, password, create_datetime) 
+VALUES ('John Doe', 'john.doe@example.com', 'password123', CURRENT_TIMESTAMP);
+INSERT INTO users (fullname, email, password, create_datetime) 
+VALUES ('Jane Smith', 'jane.smith@example.com', 'password456', '2023-05-11 10:30:00');
